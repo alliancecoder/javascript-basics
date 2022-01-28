8d2c9a16421b3e2a5ae097dfdb2ecc34a61fb456
alliancecoder <sboyd@electricaltrainingalliance.org>
2022-01-28 14:27:39 -0500

    String by value not ref

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
-// String Properties and Indexes
-console.log('Pepperoni'.length); // 9
-console.log('Pepperoni'['length']); // 9
-console.log('Pepperoni'[0]); // 9
+// String Immutability
+const immutableString = 'I shall not change!';
+const uppercaseString = immutableString.toUpperCase();
+
+console.log(immutableString, uppercaseString);
+
+// exmaple proof of by copy
+const anotherImmutable = "lowercase";
+console.log(anotherImmutable.toUpperCase(), anotherImmutable); // the orignal isn't altered just the representation.
