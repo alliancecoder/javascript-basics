d7035579848ab2c509c685970585731db6cf05af
alliancecoder <sboyd@electricaltrainingalliance.org>
2022-01-28 12:15:25 -0500

    number immutable pass by value

--- src/index.js
+++ src/index.js
@@ -4,17 +4,10 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
-// NaN (not a number)
-const result = Number('55px');
-
-// isNaN (tests for NaN ergo brokenish)
-console.log(isNaN(result)); // true
-console.log(isNaN('I am a string!')); // true
-
-// updated method of Number checks for nummber
-console.log(Number.isNaN(result)); // true
-console.log(Number.isNaN('I am a string!')); // false
-
-// other methods of Number
-// Number.isInteger()
-// Number.NaN returns NaN locally scoped not Globally scoped.
+// imutableNumbers
+// Numbers are Copied by Value, not ref
+const immutableNumber = 99; // never touched
+let referenceNumber = immutableNumber;
+referenceNumber = 44;
+console.log(immutableNumber);
+console.log(referenceNumber);
