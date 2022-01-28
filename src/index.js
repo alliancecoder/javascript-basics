acd9614fdc1b793691cec7ca6223a3a084997b9c
alliancecoder <sboyd@electricaltrainingalliance.org>
2022-01-28 13:50:26 -0500

    String Intro

--- src/index.js
+++ src/index.js
@@ -4,8 +4,30 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
-// Number Methods
-console.log(Number.prototype);
-console.log(99..toFixed(4)); // double dot fixes Identifier directly after number (99).toFixed(4) also works "99.0000"
-console.log(parseFloat((99.123456789).toFixed(4))); // string to actual number 99.1235
-console.log(parseFloat((99.123456789).toPrecision(5))); // string to actual number 99.123
+// String Literal - preferred
+console.log(String.prototype);
+
+console.log('Pizza');
+console.log("Pizza"); // same
+console.log('Pizza \'is hot\'');  // escaping
+console.log('Pizza "is hot"');
+
+const pizza = 'Pepperoni';
+console.log('Pizza is ' + pizza); // concat
+console.log(`Pizza is ${pizza}!`); // same using ` es6
+console.log(`
+Pizza is:
+${pizza}!
+`); // multi-line is preserved
+
+// Function and Constructor Syntax
+console.log(String(55 + 11)); // 66
+console.log(String(55 + '11')); // 5511 String > Number
+
+console.log(new String(55));
+/**
+ * String { "55" }
+ *      0: "5"
+ *      1: "5"
+ *      length: 2
+ */
