a9cfa3f4f60d3c390a94ecb5aad6dfb6cb0a28ed
alliancecoder <sboyd@electricaltrainingalliance.org>
2022-01-28 14:58:05 -0500

    methods

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
-// String Immutability
-const immutableString = 'I shall not change!';
-const uppercaseString = immutableString.toUpperCase();
+// String Methods
+console.log(String.prototype);
 
-console.log(immutableString, uppercaseString);
+console.log('Pepperoni'.indexOf('P'));
+console.log('Pepperoni'.includes('P'));
 
-// exmaple proof of by copy
-const anotherImmutable = "lowercase";
-console.log(anotherImmutable.toUpperCase(), anotherImmutable); // the orignal isn't altered just the representation.
+console.log('Pepperoni'.replace('oni', 'fire'));
+console.log('Pepperoni'.replace(/oni$/, 'fire'));
+
+console.log('Pepperoni'.slice(2, -3));
+
+console.log('Pepperoni~Plain'.split('~')[1]);
+console.log('Pepperoni~Plain'.split(/~/)[1]);
+
+console.log('   4444 5555  ');
