4291a52e1a5104c71ccfac40ac6fe916986b6bfa
alliancecoder <sboyd@electricaltrainingalliance.org>
2022-01-28 13:05:34 -0500

    typechecking numbers

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
-// imutableNumbers
-// Numbers are Copied by Value, not ref
-const immutableNumber = 99; // never touched
-let referenceNumber = immutableNumber;
-referenceNumber = 44;
-console.log(immutableNumber);
-console.log(referenceNumber);
+// Type Checking
+console.log(typeof 99); // number
+// note it is a string type
+const value = 99;
+if (typeof value === 'number') {
+    console.log('TRUE: Though a constant like 99 would cause an eslint error e.g., true is always true 99 is obviously a number');
+}
+
+// other methods
+// -- instanceof
+console.log(99 instanceof Number); // false it is a literal value 
+console.log(new Number('99') instanceof Number); // true Number { 99 }
+
+// Object.toString
+console.log({}.toString()); // [object Object]
+console.log(Object.prototype.toString()); // [object Object]
+console.log(Object.prototype.toString.call(99)); // [object Number] safest way
+console.log(Object.prototype.toString.call(99).slice(8, -1) === 'Number'); // true
