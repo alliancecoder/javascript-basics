d32660ad96e1bd48a88dda9dc915170444c4b263
alliancecoder <sboyd@electricaltrainingalliance.org>
2022-01-28 13:28:58 -0500

    Methods of the Number object

--- src/index.js
+++ src/index.js
@@ -4,21 +4,8 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
-// Type Checking
-console.log(typeof 99); // number
-// note it is a string type
-const value = 99;
-if (typeof value === 'number') {
-    console.log('TRUE: Though a constant like 99 would cause an eslint error e.g., true is always true 99 is obviously a number');
-}
-
-// other methods
-// -- instanceof
-console.log(99 instanceof Number); // false it is a literal value 
-console.log(new Number('99') instanceof Number); // true Number { 99 }
-
-// Object.toString
-console.log({}.toString()); // [object Object]
-console.log(Object.prototype.toString()); // [object Object]
-console.log(Object.prototype.toString.call(99)); // [object Number] safest way
-console.log(Object.prototype.toString.call(99).slice(8, -1) === 'Number'); // true
+// Number Methods
+console.log(Number.prototype);
+console.log(99..toFixed(4)); // double dot fixes Identifier directly after number (99).toFixed(4) also works "99.0000"
+console.log(parseFloat((99.123456789).toFixed(4))); // string to actual number 99.1235
+console.log(parseFloat((99.123456789).toPrecision(5))); // string to actual number 99.123
