e7e357fd4a9b9b3a25d4785a7fc0d0e6a5334075
alliancecoder <sboyd@electricaltrainingalliance.org>
2022-01-28 16:55:21 -0500

    params and defaults

--- src/index.js
+++ src/index.js
@@ -4,31 +4,21 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // --------------------------------------------------
-// Functions
+// Function Parameters and Defaults
 // --------------------------------------------------
 
-// hoisting tests
-console.log(makeCar);
-console.log(makeCarExpression);
-console.log(makeCarArrow);
-console.log(makeCarShorthand);
-
-// Option One declaration
-function makeCar() {
-    console.log('Making car...');
+// name = parameter
+// the = 'Oldsmobile' provides and es6 default
+function makeCar(name = 'Oldsmobile') {
+    // defaults in block
+    // name = name || 'Oldsmobile';
+    // if (!name) {
+    //     name = 'Oldsmobile';
+    // }
+    console.log(`Making car: ${name.toUpperCase()}`);
 }
-// calling function
+
+// strings = arguments
+makeCar('Porsche');
+makeCar('Ferrari');
 makeCar();
-
-// Option Two Function Expression (anonymous or named versions)
-const makeCarExpression = function () { }
-console.log(makeCarExpression.name);
-
-// Option Three Arrow Function
-const makeCarArrow = () => {
-    console.log('Making car inside Arrow...');
-};
-makeCarArrow();
-
-const makeCarShorthand = () => console.log('Short');
-makeCarShorthand();
