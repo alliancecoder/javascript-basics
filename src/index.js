d9ea8fa8cb22656fe40ceccfe9d39eaf50ed919c
alliancecoder <sboyd@electricaltrainingalliance.org>
2022-01-31 14:21:44 -0500

    Returns - refacterd but not done

--- src/index.js
+++ src/index.js
@@ -7,11 +7,9 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 // Function Rerurn values
 // --------------------------------------------------
 
-function makeCarPrice(...params) {
-    const total = params.reduce((prev, next) => prev + next)
-    console.log(total);
-    return; // <- this is an implicit return which returns undefined because we aren't telling it what to return.
+const makeCarPrice = (...params) => {
+    return params.reduce((prev, next) => prev + next);
 }
 
 const totalPrice = makeCarPrice(11, 22, 33, 44, 55);
-console.log(totalPrice); //undefined
+console.log(`Total Price: $${totalPrice.toFixed(2)}`); // Total Price: $165.00
