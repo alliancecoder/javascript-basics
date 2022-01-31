84921816750399da9d5e0304f04d536c33988c32
alliancecoder <sboyd@electricaltrainingalliance.org>
2022-01-31 14:22:24 -0500

    Returns - refacterd simplest form - function cleaner than arrow but preference

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 // Function Rerurn values
 // --------------------------------------------------
 
-const makeCarPrice = (...params) => {
-    return params.reduce((prev, next) => prev + next);
-}
+const makeCarPrice = (...params) => params.reduce((prev, next) => prev + next);
 
 const totalPrice = makeCarPrice(11, 22, 33, 44, 55);
 console.log(`Total Price: $${totalPrice.toFixed(2)}`); // Total Price: $165.00
