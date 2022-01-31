8880c07b76b03d47be7a2d320939606905aef6c2
alliancecoder <sboyd@electricaltrainingalliance.org>
2022-01-31 14:33:46 -0500

    Returns - call inline function and use return value

--- src/index.js
+++ src/index.js
@@ -7,7 +7,12 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 // Function Rerurn values
 // --------------------------------------------------
 
-const makeCarPrice = (...params) => params.reduce((prev, next) => prev + next);
+function makeCarPrice(...params) {
+    const total = params.reduce((prev, next) => prev + next);
+    return total;
+}
+const makeCarPriceArrow = (...params) => params.reduce((prev, next) => prev + next);
 
-const totalPrice = makeCarPrice(11, 22, 33, 44, 55);
-console.log(`Total Price: $${totalPrice.toFixed(2)}`); // Total Price: $165.00
+// can call functions in backticks
+console.log(`Total Price: $${makeCarPrice(11, 22, 33, 44, 55).toFixed(2)}`); // Total Price: $165.00
+console.log(`Total Price: $${makeCarPriceArrow(99, 77, 44).toFixed(2)}`); // Total Price: $220.00
