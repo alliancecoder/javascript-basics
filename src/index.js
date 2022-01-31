75d4a6574f2250d2cc3053dd58cf5e0352a05dfb
alliancecoder <sboyd@electricaltrainingalliance.org>
2022-01-31 15:01:09 -0500

    socpe visibility

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // --------------------------------------------------
-// Function Rerurn values
+// Function Scope
 // --------------------------------------------------
 
-function makeCarPrice(...params) {
-    const total = params.reduce((prev, next) => prev + next);
-    return total;
-}
-const makeCarPriceArrow = (...params) => params.reduce((prev, next) => prev + next);
+// scope 1 visible in file
+const anotherId = '1234abcd';
 
-// can call functions in backticks
-console.log(`Total Price: $${makeCarPrice(11, 22, 33, 44, 55).toFixed(2)}`); // Total Price: $165.00
-console.log(`Total Price: $${makeCarPriceArrow(99, 77, 44).toFixed(2)}`); // Total Price: $220.00
+
+function makeCarPartId(id) {
+    // scope 2 visible in function only
+    console.log(id, anotherId);
+}
+
+makeCarPartId('x90wece998');
