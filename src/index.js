14dce187ca58db4132fa24696d06cfec2bfbd565
alliancecoder <sboyd@electricaltrainingalliance.org>
2022-01-31 17:04:33 -0500

    internal function returned for replicated calls

--- src/index.js
+++ src/index.js
@@ -9,8 +9,20 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 function makeCarPartId(id) {
     const theId = `CAR_PART_${id}`;
-    return theId;
+    // internal function has access to the function scoped vars.
+    return function(name) {
+        return `${theId}_${name.toUpperCase()}`
+    };
 }
 
 const carPartId = makeCarPartId('x90wece998');
-console.log(carPartId);
+// the function returns access to the inner function so we can call both as a cascading call
+console.log(carPartId('Left_Door')); 
+console.log(carPartId('Right_Door')); 
+console.log(carPartId('Windscreen')); 
+
+const anotherCarPartId = makeCarPartId('d20399df');
+// the function returns access to the inner function so we can call both as a cascading call
+console.log(anotherCarPartId('Left_Door')); 
+console.log(anotherCarPartId('Right_Door')); 
+console.log(anotherCarPartId('Windscreen')); 
