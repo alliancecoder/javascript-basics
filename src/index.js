6b4da58418a524a82901c8d558afb91ece160062
alliancecoder <sboyd@electricaltrainingalliance.org>
2022-01-31 20:05:01 -0500

    binding this

--- src/index.js
+++ src/index.js
@@ -9,10 +9,16 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 const firstCar = { id: 'XAT5416' };
 const secondCar = { id: 'XAT5417' };
+const thirdCar = { id: 'XAT5418' };
 
 function carPartId(name, quantity) {
     console.log(`${this.id}_${name}_${quantity}`);
 }
+
+const boundThirdCar = carPartId.bind(thirdCar);
+boundThirdCar('WINDSCREEN', 99);
+
+// binding anchors the 'this' contxt to the new method
 
 // The call() method calls a function with a given this value and arguments provided individually. MDN
 // call = c = commas
