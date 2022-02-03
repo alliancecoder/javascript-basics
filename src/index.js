8bb4ea45a51935a787b2914aa447dbb96058379b
alliancecoder <sboyd@electricaltrainingalliance.org>
2022-02-03 18:37:01 -0500

    simple and complex properties

--- src/index.js
+++ src/index.js
@@ -22,9 +22,11 @@ console.log(drink.getDrinkDetails());
 
 // logging properties uses the key
 console.log(drink['complex property']);
+console.log(drink.name);
 
 /**
  * KV Pairs are stored like 'key': 'value' so both works
  * the use of the space requires the '' within the key
- * logging properties uses the key
+ * logging complex properties uses the key
+ * logging simple keys can use the key or dot notation
  */
