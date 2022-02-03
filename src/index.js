67e6abfda127581262c036ef3e128f17917db3b4
alliancecoder <sboyd@electricaltrainingalliance.org>
2022-02-03 17:13:39 -0500

    object literal defined

--- src/index.js
+++ src/index.js
@@ -4,24 +4,14 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // --------------------------------------------------
-// Functions Contect (this)
+// Object Literal, Function and Constructor Syntax
 // --------------------------------------------------
 
-const firstCar = { id: 'XAT5416' };
-const secondCar = { id: 'XAT5417' };
-const thirdCar = { id: 'XAT5418' };
+// object literal
+const drink = {
+    id: 'XAT5621',
+    name: 'Root Beer',
+    price: 99
+};
 
-function carPartId(name, quantity) {
-    console.log(`${this.id}_${name}_${quantity}`);
-}
-
-const boundThirdCar = carPartId.bind(thirdCar);
-boundThirdCar('WINDSCREEN', 99);
-
-// binding anchors the 'this' contxt to the new method
-
-// The call() method calls a function with a given this value and arguments provided individually. MDN
-// call = c = commas
-carPartId.call(firstCar, 'LEFT_DOOR', 12); // first param this, second passes
-// apply = a = array
-carPartId.apply(secondCar, ['RIGHT_DOOR', 21]); // first this, must be an array for second arg
+console.log(drink);
